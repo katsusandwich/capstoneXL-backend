1ab6134a0b52debf9a8948cc5f0b26df1b66f46e
KATSUSANDWICH <MEOWSPOKENHERE@GMAIL.COM>
2022-12-18 11:17:52 +0800

    changed casing for words

--- db/migrations/20221217095502-create-word.js
+++ db/migrations/20221217095502-create-word.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Words", {
+    await queryInterface.createTable("words", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -54,6 +54,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Words");
+    await queryInterface.dropTable("words");
   },
 };
