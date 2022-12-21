67e351c70fb3c06d8c097b20a918445e87836787
KATSUSANDWICH <MEOWSPOKENHERE@GMAIL.COM>
2022-12-21 12:56:39 +0800

    first draft of index.js

--- index.js
+++ index.js
@@ -9,14 +9,10 @@ const app = express();
 
 // importing Routers
 const UsersRouter = require("./routers/usersRouter");
-const ScoresRouter = require("./routers/scoresRouter");
-const WordlistsRouter = require("./routers/wordlistsRouter");
 const WordsRouter = require("./routers/wordsRouter");
 
 // importing Controllers
 const UsersController = require("./controllers/usersController");
-const ScoresController = require("./controllers/scoresController");
-const WordlistsController = require("./controllers/wordlistsController");
 const WordsController = require("./controllers/wordsController");
 
 // importing DB
@@ -24,59 +20,14 @@ const db = require("./db/models/index");
 const { score, user, word, wordlist } = db;
 
 // initializing Controllers -> note the lowercase for the first word
-const fabricsController = new FabricsController(fabric);
-const collarsController = new CollarsController(collar);
-const backsController = new BacksController(back);
-const cuffsController = new CuffsController(cuff);
-const frontsController = new FrontsController(front);
-const pocketsController = new PocketsController(pocket);
 
-const usersController = new UsersController(
-  user,
-  measurement,
-  wishlist,
-  fabric,
-  cuff,
-  collar,
-  back,
-  front,
-  pocket,
-  shoppingCart
-);
+const usersController = new UsersController(user, score, wordlist, word);
 
-const orderController = new OrderController(
-  order,
-  user,
-  measurement,
-  order_detail,
-  fabric,
-  cuff,
-  collar,
-  back,
-  front,
-  pocket,
-  shoppingCart
-);
-const orderDetailsController = new OrderDetailsController(order_detail);
-const wishlistsController = new WishlistsController(wishlist);
+const wordsController = new WordsController(user, score, wordlist, word);
 
-// inittializing Routers
-const fabricRouter = new FabricsRouter(fabricsController).routes();
-const collarRouter = new CollarsRouter(collarsController).routes();
-const backRouter = new BacksRouter(backsController).routes();
-const cuffRouter = new CuffsRouter(cuffsController).routes();
-const frontRouter = new FrontsRouter(frontsController).routes();
-const pocketRouter = new PocketsRouter(pocketsController).routes();
-
+// initializing Routers
 const userRouter = new UsersRouter(usersController, auth).routes();
-// const userRouter = new UsersRouter(usersController).routes();
-
-const orderRouter = new OrderRouter(orderController, auth).routes();
-const orderDetailsRouter = new OrderDetailsRouter(
-  orderDetailsController
-).routes();
-
-const wishlistRouter = new WishlistsRouter(wishlistsController).routes();
+const wordRouter = new WordsRouter(wordsController, auth).routes();
 
 // Enable CORS access to this server
 app.use(cors());
@@ -85,17 +36,8 @@ app.use(cors());
 app.use(express.json());
 
 // using the routers
-app.use("/fabrics", fabricRouter);
-app.use("/collars", collarRouter);
-app.use("/backs", backRouter);
-app.use("/cuffs", cuffRouter);
-app.use("/fronts", frontRouter);
-app.use("/pockets", pocketRouter);
-
 app.use("/users", userRouter);
-
-app.use("/orders", orderRouter);
-app.use("/wishlists", wishlistRouter);
+app.use("/:userId/wordlists/:wordlistId/", wordRouter);
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
@@ -104,7 +46,3 @@ app.listen(PORT, () => {
 // app.get("/", (req, res) => {
 //   res.send("Hello, World!");
 // });
-
-app.listen(PORT, () => {
-  console.log(`Express app listening on port ${PORT}!`);
-});
